cda4c72b0777f2254d0da416987f370e41e397fa
Gabriel Rodrigues <grsou3@minha.fag.edu.br>
2020-03-30 01:36:59 -0300

    remove conect timeout

--- server.js
+++ server.js
@@ -7,20 +7,14 @@ const timeout = require('connect-timeout')
 const app = express();
 app.use(express.json());
 
-app.use(timeout('40s'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(haltOnTimedout)
-app.use(haltOnTimedout)
 app.use(cors());
 
 
 //todos os tipo de requisições são redirecionados para o arquivos de rotas
 app.use("/api", require("./app/routes"));
 
-function haltOnTimedout (req, res, next) {
-  if (!req.timedout) next()
-}
 
 //Servidor irá ficar "ouvindo" a porta 3000
 app.listen(process.env.PORT || 3001);
