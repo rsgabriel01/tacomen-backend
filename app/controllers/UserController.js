255352031d496bf10dd27a648428ab08cad29e26
Gabriel Rodrigues <grsou3@minha.fag.edu.br>
2020-03-29 22:42:50 -0300

    log route users

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -5,9 +5,9 @@ module.exports = {
   async allUsers(req, res) {
     console.log('acessado users');
     
-    try {
-      const users = await user.findAll();
+    const users = await user.findAll();
 
+    try {
       return res.json(users);
     } catch (error) {
       console.log(error);
